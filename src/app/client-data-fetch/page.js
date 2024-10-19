83bcb8acae805874c53bee8b28b89f89d0e40f6d
napharsh <harshgoel18122001@gmail.com>
2024-10-19 11:18:41 +0530

    Client Data fetching and displahing users and their details as well , managing the loading state as well

--- src/app/client-data-fetch/page.js
+++ src/app/client-data-fetch/page.js
@@ -1,13 +1,47 @@
 'use client'
 
+import Link from "next/link";
+import { useEffect, useState } from "react"
+
 // use effect 
 // ShadowRoot, useSwr hook
 // with loading state 
 
 export default function ClientDataFetching(){
+    const [loading, setLoading] = useState(false);
+    const [users, setUsers] = useState([]);
+
+    async function  fetchListOfUsers() {
+        try {
+            setLoading(true);
+            const response = await fetch('https://dummyjson.com/users');
+            const result = await response.json();
+            if(result?.users){
+                setUsers(result.users);
+                setLoading(false);
+            }   
+        } catch (error) {
+            setUsers([]);
+            setLoading(false);
+        }
+    }
+    useEffect(()=>{
+        fetchListOfUsers();
+    },[])
+    if(loading){
+        return <h1 className="text-3xl font-bold">Loading users, pls wait...</h1>
+    }
     return (
         <>
             <h1>Client side data fetching</h1>
+            <ul>
+              {
+                  users && users.length > 0 ? users.map(user => <li>
+                    <Link href={`server-data-fetch/${user.id}`}>{user.firstName}</Link>
+                  </li>) : null
+              }
+
+            </ul>
         </>
     )
 }
